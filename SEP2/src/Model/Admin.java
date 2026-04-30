76fa9f1425d1d398c2b86818598d485606162163
Leon210109 <144994653+Leon210109@users.noreply.github.com>
2026-04-30 09:58:39 +0200

    Add files via upload

---
+++ SEP2/src/Model/Admin.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Admin
+{
+  private String ID;
+  private String username;
+  private String password;
+
+  public Admin(String ID, String username, String password)
+  {
+    this.ID = ID;
+    this.username = username;
+    this.password = password;
+  }
+}
